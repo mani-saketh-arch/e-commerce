82689792e9e31ea560f896232e19bd17ab472a32
GANDHAM MANI <gandhammani2421@gmail.com>
2025-11-21 02:04:46 +0530

    Added admin cors

--- app/main.py
+++ app/main.py
@@ -75,6 +75,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://arw-merchandice.netlify.app",
+        "https://arw-merchandice-admin.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],

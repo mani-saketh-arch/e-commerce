c33566ff43f8f4d12e79c8f9306064faf49c1bc9
GANDHAM MANI <gandhammani2421@gmail.com>
2025-11-21 01:34:14 +0530

    Fix CORS - allow Netlify frontend

--- app/main.py
+++ app/main.py
@@ -74,6 +74,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://arw-merchandice.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
